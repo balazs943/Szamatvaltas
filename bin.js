6a75eab7c48081dc2d94f3847e16047914921849
Lezsi <lezsidiscord@gmail.com>
2025-02-24 12:15:48 +0100

    Add files via upload

--- bin.js
+++ bin.js
@@ -1,21 +1,105 @@
-var x = document.getElementById("feladat1")
 
 function funkcio(){
 
 
 
     var x = document.getElementById("feladat1").value
-
-    
-
-
-
+    if (x == "1010"){
+        document.getElementById("megoldas1").innerHTML = "Helyes"
+    }
+    else{
+        document.getElementById("megoldas1").innerHTML = "Helytelen"
+    }
 
 }
 
     
+function funkcio2(){
 
 
+
+    var x = document.getElementById("feladat2").value
+    if (x == "1011111100"){
+        document.getElementById("megoldas2").innerHTML = "Helyes"
+    }
+    else{
+        document.getElementById("megoldas2").innerHTML = "Helytelen"
+    }
+
+}
+
+
+function funkcio3(){
+
+
+
+    var x = document.getElementById("feladat3").value
+    if (x == "11000011100"){
+        document.getElementById("megoldas3").innerHTML = "Helyes"
+    }
+    else{
+        document.getElementById("megoldas3").innerHTML = "Helytelen"
+    }
+
+}
+
+function funkcio4(){
+
+
+
+    var x = document.getElementById("feladat4").value
+    if (x == "10101010110100001"){
+        document.getElementById("megoldas4").innerHTML = "Helyes"
+    }
+    else{
+        document.getElementById("megoldas4").innerHTML = "Helytelen"
+    }
+
+}
+
+function funkcio5(){
+
+
+
+    var x = document.getElementById("feladat5").value
+    if (x == "110("){
+        document.getElementById("megoldas5").innerHTML = "Helyes"
+    }
+    else{
+        document.getElementById("megoldas5").innerHTML = "Helytelen"
+    }
+
+}
+
+function funkcio6(){
+
+
+
+    var x = document.getElementById("feladat6").value
+    if (x == "100001111100100000011"){
+        document.getElementById("megoldas6").innerHTML = "Helyes"
+    }
+    else{
+        document.getElementById("megoldas6").innerHTML = "Helytelen"
+    }
+
+}
+
+
+
+function funkcio7(){
+
+
+
+    var x = document.getElementById("feladat7").value
+    if (x == "1011101001000011010010101111100100111"){
+        document.getElementById("megoldas7").innerHTML = "Helyes"
+    }
+    else{
+        document.getElementById("megoldas7").innerHTML = "Helytelen"
+    }
+
+}
 
 
 
